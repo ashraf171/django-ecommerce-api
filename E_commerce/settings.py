d474ce0c85f60e80b679582473d99628be3f0810
Ashraf almouaie <ashrafalmouaie>
2026-06-12 08:13:09 -0500

    Add static files configuration

--- E_commerce/settings.py
+++ E_commerce/settings.py
@@ -157,7 +157,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -166,5 +165,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
-MEDIA_URL='/media/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
+MEDIA_URL = '/media/'
 MEDIA_ROOT=BASE_DIR / 'media'
